5154a84a8eda9279e789ded89590385aed2541c7
Peace Munyai <47421951+PeaceTheeCoder@users.noreply.github.com>
2021-07-30 00:40:54 +0200

    Update settings.py

--- chatroomAPI/settings.py
+++ chatroomAPI/settings.py
@@ -16,14 +16,15 @@ import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f7-5^g58$f-94n&3rxv9vd7k6u=2zmiic7z58^-mvo@ot@-!4b'
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
