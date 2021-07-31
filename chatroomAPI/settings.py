68ffed85e69b52a7de1219873a15e9bbb540bbc7
Peace Munyai <sapeace1@gmail.com>
2021-07-31 13:02:19 +0200

    resrting main to before production

--- chatroomAPI/settings.py
+++ chatroomAPI/settings.py
@@ -23,13 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-f7-5^g58$f-94n&3rxv9vd7k6u=2zmiic7z58^-mvo@ot@-!4b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['strrikecode-chatroom-api.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
